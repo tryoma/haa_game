2458e1ef01e3e5dbc29caf4319d084ee7b0426b5
tryoma <tryoma0326@gmail.com>
2023-02-22 16:27:48 +0900

    test

--- src/controllers/line.ts
+++ src/controllers/line.ts
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { RequestHandler } from 'express';
 import * as line from '@line/bot-sdk';
 
