9bc2a728542a208a168bc35bd586699ca5c052b8
tryoma <tryoma0326@gmail.com>
2023-02-26 12:23:34 +0900

    test

--- src/controllers/line.ts
+++ src/controllers/line.ts
@@ -43,7 +43,6 @@ export const lineEndpoint: RequestHandler = async (req, res, next) => {
   const event = req.body.events[0];
   if (event.type === 'message' && event.message.type === 'text') {
     if (event.message.text === '新規') {
-      // console.log(records[0]);
       const titleId = records[1 + Math.floor(Math.random() * (records.length - 1))][0];
       const uniqId =
         Math.floor(Math.random() * 101) +
@@ -61,10 +60,8 @@ export const lineEndpoint: RequestHandler = async (req, res, next) => {
           console.log('err');
           console.log(err);
         } else {
-          console.log(data);
-          console.log(Number(data.titleId));
           const titleId = Number(data.titleId);
-          const selectRecord = records[titleId];
+          const selectRecord = records[titleId - 1];
           client.replyMessage(event.replyToken, [
             textTemplate(`あなたの番号は『${newNum}』`),
             textTemplate(`お題IDは『${data.uniqId}』`),
@@ -87,38 +84,37 @@ export const lineEndpoint: RequestHandler = async (req, res, next) => {
       const data = await DataModel.findOne({ uniqId: event.message.text });
       if (data) {
         const array = data.usedNumber;
-        console.log('array')
-        console.log(array)
-        let newNum = Math.floor(1 + Math.random() * 8);
-        console.log('array')
-        console.log(array)
-        console.log('newNum')
-        console.log(newNum)
-        console.log('array.includes(newNum)')
-        console.log(array.includes(newNum))
-        while (array.includes(newNum)) {
-          newNum = Math.floor(1 + Math.random() * 8);
-        }
-        const newArray = [...array, newNum];
-        await DataModel.updateOne(
-          { uniqId: lineText },
-          {
-            $set: {
-              usedNumber: newArray,
-            },
+        if (array.length <= 8) {
+          let newNum = Math.floor(1 + Math.random() * 8);
+          while (array.includes(newNum)) {
+            newNum = Math.floor(1 + Math.random() * 8);
           }
-        );
-        const titleId = Number(data.titleId);
-        const selectRecord = records[titleId];
-        client.replyMessage(event.replyToken, [
-          textTemplate(`あなたの番号は『${newNum}』`),
-          textTemplate(
-            `お題は『${selectRecord[1]}』\n1. ${selectRecord[2]}\n2. ${selectRecord[3]}\n3. ${selectRecord[4]}\n4. ${selectRecord[5]}\n5. ${selectRecord[6]}\n6. ${selectRecord[7]}\n7. ${selectRecord[8]}\n8. ${selectRecord[9]}`
-          ),
-          textTemplate(
-            `コピーしてメモにつかってください。\n1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. `
-          ),
-        ]);
+          const newArray = [...array, newNum];
+          await DataModel.updateOne(
+            { uniqId: lineText },
+            {
+              $set: {
+                usedNumber: newArray,
+              },
+            }
+          );
+          const titleId = Number(data.titleId);
+          const selectRecord = records[titleId - 1];
+          client.replyMessage(event.replyToken, [
+            textTemplate(`あなたの番号は『${newNum}』`),
+            textTemplate(
+              `お題は『${selectRecord[1]}』\n1. ${selectRecord[2]}\n2. ${selectRecord[3]}\n3. ${selectRecord[4]}\n4. ${selectRecord[5]}\n5. ${selectRecord[6]}\n6. ${selectRecord[7]}\n7. ${selectRecord[8]}\n8. ${selectRecord[9]}`
+            ),
+            textTemplate(
+              `コピーしてメモにつかってください。\n1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. `
+            ),
+          ]);
+        } else {
+          client.replyMessage(
+            event.replyToken,
+            textTemplate('人数は8人までです。')
+          );
+        }
       }
     } else {
       client.replyMessage(event.replyToken, confirmTemplate());
